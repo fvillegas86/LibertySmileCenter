c036707316e42b2ab43dab8f2ce1adf7ea39af15
Fernando Mac Mini <hello@innovexitsolutions.com>
2026-01-08 17:47:56 -0800

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import partytown from '@astrojs/partytown';
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [partytown({
+    config: {
+      forward: ['dataLayer.push'],
+    },
+  }), icon()]
+});
